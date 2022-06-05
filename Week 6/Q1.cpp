9adfcb8da8b3e7bb57fabc8ea2737bbbfbe7cf14
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-06-05 15:57:53 +0530

    Week6 Q1

---
+++ Week 6/Q1.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <list>
+#include <queue>
+using namespace std;
+
+class Graph
+{
+    int V;
+    list<int> *l;
+public:
+    Graph(int v)
+    {
+        V = v;
+        l = new list<int>[V];
+    }
+    void addEdge(int i, int j, bool undir = false)   // undir == false for directed graph
+    {
+        l[i].push_back(j);
+        if(undir){
+            l[j].push_back(i);
+        }
+    }
+    bool isReachable(int source, int destination)
+    {
+        if(source == destination)
+            return true;
+        queue<int> q;
+        bool *visited = new bool[V]{0};
+        q.push(source);
+        visited[source] = true;
+        while(!q.empty()){
+            // Do some work for every node
+            int f = q.front();
+            cout << f << endl;
+            q.pop();
+
+            //Push the nbrs of current node inside q if they are not already visited 
+            for(auto nbr : l[f])
+            {
+                if(nbr == destination)
+                    return true;
+                if(!visited[nbr])
+                {
+                    q.push(nbr);
+                    visited[nbr] = true;
+                }
+            }
+        }
+        return false;
+    }
+
+};
+int main()
+{
+    Graph g(7);
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(2,3);
+    g.addEdge(3,5);
+    g.addEdge(5,6);
+    g.addEdge(4,5);
+    g.addEdge(0,4);
+    g.addEdge(3,4);
+    if(g.isReachable(1, 5))
+        cout << "1 is reachable from 5 is true " << endl;
+    else
+        cout << "1 is reachable from 5 is false" << endl;
+    return 0;
+}
